c1dc72897b0363be7e504e225ba2708391b55334
And4Web <anands2807@gmail.com>
2022-07-26 08:25:44 +0530

    created 4 different pages and urls

--- learningDjangoWithCWH/home/views.py
+++ learningDjangoWithCWH/home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("Hello, this is my first django project")
+
+def about(request):
+  return HttpResponse("Hello, this is About page")
+
+def services(request):
+  return HttpResponse("Hello, this is Services page")
+
+def contact(request):
+  return HttpResponse("Hello, this is Contact page")
