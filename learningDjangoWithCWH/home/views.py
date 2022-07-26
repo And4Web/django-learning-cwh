0bfb3bf6d795dab755cddc55996d5c0184c706fd
And4Web <anands2807@gmail.com>
2022-07-26 11:46:42 +0530

    created static file serving and templates to send file to the client as well as with some variables.

--- learningDjangoWithCWH/home/views.py
+++ learningDjangoWithCWH/home/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, this is my first django project")
+  context = {
+    "variable1": "Python is Great",
+    "variable2": "Javascript is also Great"
+  }
+  return render(request, "index.html", context)
+  # return HttpResponse("Hello, this is my first django project")
 
 def about(request):
   return HttpResponse("Hello, this is About page")
