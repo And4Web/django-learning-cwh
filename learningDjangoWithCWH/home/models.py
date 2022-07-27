a41d68ad9bff50c663abf024841624ef2131046b
And4Web <anands2807@gmail.com>
2022-07-27 20:27:52 +0530

    created and registered Contact model

--- learningDjangoWithCWH/home/models.py
+++ learningDjangoWithCWH/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+  name = models.CharField(max_length=50)
+  email = models.CharField(max_length=150)
+  phone = models.CharField(max_length=20)
+  #password = models.CharField
+  description = models.TextField(max_length=1000)
+  created_at = models.DateTimeField
